f8ee932d06de87f08b9e0f408496efc48761573e
John Ellis <john.ellis1392@gmail.com>
2017-07-03 17:47:06 -0700

    Update install script

--- packages/install.sh
+++ packages/install.sh
@@ -4,7 +4,7 @@ echo "Installing packages..."
 PACKAGE_FILE=packages.txt
 PACKAGES=$(cat ${PACKAGE_FILE})
 for package in ${PACKAGES}; do
-  if [[ ! -n "${package}" ]]; then
+  if [[ -n "${package}" ]]; then
 	echo "+ Installing package '${package}'"
 	apm install ${package}
   fi
